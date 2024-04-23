114fcd4af8984f8f60f0e18acad0d4cfa543f5ab
Camden Withrow <withrowcamden@gmail.com>
2024-04-23 15:36:55 -0500

    colorscheme

--- neovim/.config/nvim/lua/camden/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/camden/plugins/colorscheme.lua
@@ -1,13 +1,14 @@
 return {
   {
-    "aktersnurra/no-clown-fiesta.nvim",
-    -- "kvrohit/mellow.nvim",
-    priority = 1000,
+    "projekt0n/github-nvim-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      require("no-clown-fiesta").setup({
-        styles = {},
+      require("github-theme").setup({
+        -- ...
       })
-      vim.cmd([[colorscheme no-clown-fiesta]])
+
+      vim.cmd("colorscheme github_dark_dimmed")
     end,
   },
   -- {
